37f614085e33e7185b76d2972af944cf2b926fc8
maskudo <manujkarki101@gmail.com>
2022-05-01 18:51:44 +0545

    setup basic file structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 import App from "./App";
