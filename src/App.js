e9aad53f12eb17cc4482d01841aee1222e14cb9d
maskudo <manujkarki101@gmail.com>
2022-05-04 23:31:43 +0545

    add appwrapper component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./styles/App.css";
+import { useEffect, useContext } from "react";
+import { onAuthStateChanged } from "firebase/auth";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
-import { UserContextProvider } from "./context/UserContext";
+import { UserContextProvider, UserContext } from "./context/UserContext";
+import { auth } from "./firebase";
+import "./styles/App.css";
 
 function App() {
+  const { setUser } = useContext(UserContext);
+  useEffect(() => {
+    const unSubAuth = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      console.log(loggedUser);
+    });
+    return unSubAuth();
+  }, []);
   return (
     <BrowserRouter>
-      <UserContextProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </UserContextProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  );
+}
