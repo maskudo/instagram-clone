e3febbaf34a17188f3559d0b9104331cf6d9fcb9
maskudo <manujkarki101@gmail.com>
2022-05-05 23:04:31 +0545

    add post on form submit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,61 @@
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { useContext, useState } from "react";
+import { updateDoc, arrayUnion, doc, Timestamp } from "firebase/firestore";
 import { UserContext } from "../context/UserContext";
+import { db, storage } from "../firebase";
+
+const updateUserPosts = async (username, post) => {
+  const userRef = doc(db, "users", username);
+
+  // Atomically add a new region to the "regions" array field.
+  await updateDoc(userRef, {
+    posts: arrayUnion(post),
+  });
+};
 
 function Sidebar() {
   const { user } = useContext(UserContext);
   const [postCaption, setPostCaption] = useState();
+  const [progress, setProgress] = useState(0);
+
   const handleChange = (e) => {
-    if (e.target.files[0].size > 1024 * 1024) {
-      alert("File size should be smaller than 1 MB");
+    if (e.target.files[0].size > 1024 * 1024 * 2) {
+      alert("File size should be smaller than 2 MB");
       e.target.value = "";
     } else {
-      const url = URL.createObjectURL(e.target.files[0]);
+      console.log(e.target.files[0]);
     }
+  };
+  const OnSubmit = (e) => {
+    e.preventDefault();
+    const file = e.target[0].files[0];
+    const fileRef = ref(storage, `images/${file.name}`);
+    const uploadTask = uploadBytesResumable(fileRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        setProgress(
+          Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+        );
+      },
+      (error) => {
+        setProgress(error.message);
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const post = {
+            image: downloadURL,
+            caption: postCaption,
+            uploadTime: Timestamp.now(),
+          };
+          updateUserPosts(user.username, post);
+        });
+      }
+    );
   };
   return (
     <div className="col-4">
@@ -44,7 +89,7 @@ function Sidebar() {
               ></button>
             </div>
             <div class="modal-body">
-              <form>
+              <form onSubmit={OnSubmit}>
                 <input
                   name="picture"
                   type="file"
@@ -63,7 +108,13 @@ function Sidebar() {
                     setPostCaption(e.target.value);
                   }}
                 />
+                <input
+                  type="submit"
+                  className="btn btn-lg btn-success"
+                  value={"Upload"}
+                />
               </form>
+              <h6>Progress: {progress}</h6>
             </div>
             <div class="modal-footer">
               <button
