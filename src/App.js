c3d869e7ba0e20c5765d1854d59ab40632318c01
maskudo <manujkarki101@gmail.com>
2022-05-05 19:38:58 +0545

    get logged in user info

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useContext } from "react";
 import { onAuthStateChanged } from "firebase/auth";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import { UserContextProvider, UserContext } from "./context/UserContext";
-import { auth } from "./firebase";
+import { auth, db } from "./firebase";
 import "./styles/App.css";
 
 function App() {
   const { setUser } = useContext(UserContext);
   useEffect(() => {
     const unSubAuth = onAuthStateChanged(auth, (loggedUser) => {
-      setUser(loggedUser);
-      console.log(loggedUser);
+      const getData = async () => {
+        const colRef = collection(db, "users");
+        const q = query(colRef, where("uid", "==", `${loggedUser.uid}`));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          setUser({ ...doc.data(), username: doc.id });
+        });
+      };
+      getData();
     });
     return unSubAuth();
   }, []);
