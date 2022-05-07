c6b6affa7181256ae0ee71223d31776fda59982c
maskudo <manujkarki101@gmail.com>
2022-05-07 14:33:29 +0545

    add function to handle comment

--- src/components/common/Post.js
+++ src/components/common/Post.js
@@ -1,17 +1,49 @@
+import { db } from "../../firebase";
+import { useState, useContext } from "react";
+import { UserContext } from "../../context/UserContext";
 import Avatar from "./Avatar";
+import { arrayUnion, doc, Timestamp, updateDoc } from "firebase/firestore";
+
 function Post({ post }) {
+  const [uPost, setUPost] = useState(post);
+  const [commentText, setCommentText] = useState("");
+  const { user } = useContext(UserContext);
+  const submitComment = (e) => {
+    e.preventDefault();
+    const comment = {
+      text: commentText,
+      username: user.username,
+      createdAt: Timestamp.now(),
+    };
+    const postRef = doc(db, "posts", post.id);
+    console.log("here lol");
+    updateDoc(postRef, {
+      comments: arrayUnion(comment),
+    })
+      .then(() => {
+        post.comments.push(comment);
+        setUPost(post);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+    const form = document.getElementById(`f-${uPost.id}`);
+    form.reset();
+    setCommentText("");
+  };
   return (
     <div className="card my-4">
       <div className="card-header d-flex align-items-center">
-        <Avatar photo={post.userAvatar} size={2.5} />
+        <Avatar photo={uPost.userAvatar} size={2.5} />
         <div className="mx-4">
           <a href="/" className="nav-link">
-            {post.username}
+            {uPost.username}
           </a>
         </div>
       </div>
       <div class="card-body p-0">
-        <img src={post.image} alt="monke lol" className="img-fluid p-0" />
+        <img src={uPost.image} alt="monke lol" className="img-fluid p-0" />
       </div>
       <div className="text-start p-4">
         <div className="top-items row">
@@ -43,27 +75,47 @@ function Post({ post }) {
         </div>
         <div className="caption">
           <p>
-            <a href="/">{post.username}</a> {post.caption}
+            <a href="/">{uPost.username}</a> {uPost.caption}
           </p>
         </div>
         <div className="comment-box">
           <div className="view-comment-menu">View All Comments</div>
           <div className="comments">
-            <div className="comment">user.handle Cool pic sis</div>
+            {uPost.comments.length ? (
+              uPost.comments.map((comment) => {
+                return (
+                  <div className="comment">
+                    <p>
+                      <span className="bold">{comment.username}</span>{" "}
+                      {comment.text}
+                    </p>
+                  </div>
+                );
+              })
+            ) : (
+              <p className="bg-light">
+                No comments yet. Be the first to comment!
+              </p>
+            )}
           </div>
         </div>
       </div>
       <div className="add-comment card-footer">
-        <form className="row">
+        <form className="row" onSubmit={submitComment} id={`f-${uPost.id}`}>
           <div className="col-9 m-2">
             <input
+              name="comment"
+              required
+              onChange={(e) => {
+                setCommentText(e.target.value);
+              }}
               type="text"
               className="form-control border-0"
               placeholder="Add a comment..."
             />
           </div>
           <div className="col-2 d-flex align-items-center justify-content-end">
-            <button className="btn btn-outline-secondary " type="submit">
+            <button className="btn btn-outline-secondary" type="submit">
               Post
             </button>
           </div>
