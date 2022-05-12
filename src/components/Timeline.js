72f8ea3373b93368f33b21f325bc94a4fe0a782b
maskudo <manujkarki101@gmail.com>
2022-05-12 15:52:06 +0545

    fetch and display new posts on scroll to bottom

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
-import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from "firebase/firestore";
 import Post from "./common/Post";
 const fetchPosts = async () => {
   const postsRef = collection(db, "posts");
@@ -22,6 +31,49 @@ function Timeline() {
     };
     getPosts();
   }, []);
+  useEffect(() => {
+    const handleScroll = async () => {
+      const windowHeight =
+        "innerHeight" in window
+          ? window.innerHeight
+          : document.documentElement.offsetHeight;
+      const body = document.body;
+      const html = document.documentElement;
+      const docHeight = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
+      );
+      const windowBottom = windowHeight + window.pageYOffset;
+      if (Math.ceil(windowBottom) >= docHeight - 1) {
+        if (posts.length) {
+          const lastPost = posts[posts.length - 1];
+          const docSnap = await getDoc(
+            doc(collection(db, "posts"), lastPost.id)
+          );
+          const nextDocsQuery = query(
+            collection(db, "posts"),
+            orderBy("uploadTime", "desc"),
+            limit(3),
+            startAfter(docSnap)
+          );
+          const querySnapshot = await getDocs(nextDocsQuery);
+          let nextPosts = [];
+          querySnapshot.forEach((doc) => {
+            const newPost = { ...doc.data(), id: doc.id };
+            nextPosts.push(newPost);
+          });
+          setPosts([...posts, ...nextPosts]);
+        }
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
   return (
     <div className="col-8">
       {!!posts.length &&
