e208a5e22f33dfc835057ff74d6cb5d2efb67779
maskudo <manujkarki101@gmail.com>
2022-05-13 15:01:20 +0545

    remove duplicate post

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -291,14 +291,6 @@ function Profile() {
                           id={post.id}
                         />
                       </div>
-                      <div className="col align-items-center pt-4 pb-0 ">
-                        <img
-                          src={post.image}
-                          alt=""
-                          className="profile-post img-fluid w-100"
-                          id={post.id}
-                        />
-                      </div>
                     </>
                   );
                 })
