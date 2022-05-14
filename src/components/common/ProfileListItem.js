a02d4d6fbcd766373cfcc162e3b0a9ae32bd8fb3
maskudo <manujkarki101@gmail.com>
2022-05-14 10:49:53 +0545

    edit styles

--- src/components/common/ProfileListItem.js
+++ src/components/common/ProfileListItem.js
@@ -2,9 +2,12 @@ import { FollowUser } from "../../Functions/followUser";
 import Avatar from "./Avatar";
 function ProfileListItem({ user, profileListUser }) {
   return (
-    <div id={`p-${profileListUser.username}`} className="row pb-2">
+    <div
+      id={`p-${profileListUser.username}`}
+      className="row pb-3 d-flex align-items-center"
+    >
       <div className="col-8 align-items-center d-flex justify-content-start">
-        <Avatar photo={profileListUser.photo} size={2} />
+        <Avatar photo={profileListUser.photo} size={3} />
         <h6 className="px-2">{profileListUser.username}</h6>
       </div>
       <div className="col-4 ">
